60dfeb37fe015d06648bf5a71dd8c0f7e8209002
mokajima <mokajima.85z@gmail.com>
2018-12-19 22:34:33 +0900

    feat: Create reducers

---
+++ frontend/src/reducers/comments.js
@@ -0,0 +1,59 @@
+import {
+  RECEIVE_COMMENTS,
+  ADD_COMMENT,
+  EDIT_COMMENT,
+  DELETE_COMMENT,
+  UP_COMMENT_VOTE,
+  DOWN_COMMENT_VOTE,
+  DELETE_PARENT_POST
+} from '../actions/comments'
+
+export function comments(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_COMMENTS :
+      return {
+        ...state,
+        ...action.comments
+      }
+    case ADD_COMMENT :
+    case EDIT_COMMENT :
+      return {
+        ...state,
+        ...action.comment
+      }
+    case DELETE_COMMENT :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          deleted: true
+        }
+      }
+    case UP_COMMENT_VOTE :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          voteScore: ...state[action.id].voteScore++
+        }
+      }
+    case DOWN_COMMENT_VOTE :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          voteScore: ...state[action.id].voteScore--
+        }
+      }
+    case DELETE_PARENT_POST :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          parentDeleted: true
+        }
+      }
+    dafault :
+      return state
+  }
+}
---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,68 @@
+import {
+  RECEIVE_POSTS,
+  ADD_POST,
+  EDIT_POST,
+  DELETE_POST,
+  UP_POST_VOTE,
+  DOWN_POST_VOTE,
+  ADD_CHILD_COMMENT,
+  DELETE_CHILD_COMMENT
+} from '../actions/posts'
+
+export default function posts(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_POSTS :
+      return {
+        ...state,
+        ...action.posts
+      }
+    case ADD_POST :
+    case EDIT_POST :
+      return {
+        ...state,
+        ...action.post
+      }
+    case DELETE_POST :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          deleted: true
+        }
+      }
+    case UP_POST_VOTE :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          voteScore: ...state[action.id].voteScore++
+        }
+      }
+    case DOWN_POST_VOTE :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          voteScore: ...state[action.id].voteScore--
+        }
+      }
+    case ADD_CHILD_COMMENT :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          commentCount: ...state[action.id].commentCount++
+        }
+      }
+    case DELETE_CHILD_COMMENT :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          commentCount: ...state[action.id].commentCount--
+        }
+      }
+    default :
+      return state
+  }
+}
