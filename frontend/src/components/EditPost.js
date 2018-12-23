7b716fb5ce41f6c544c8804b8273e5a682fc7afb
mokajima <mokajima.85z@gmail.com>
2018-12-23 13:49:35 +0900

    feat: Add an EditPost component

---
+++ frontend/src/components/EditPost.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { editPost } from '../actions/posts'
+import { getTimestamp } from '../utils/helpers'
+import PostForm from './PostForm'
+
+class EditPost extends Component {
+  state = {
+    title: this.props.post.title,
+    body: this.props.post.body,
+    author: this.props.post.author,
+    category: this.props.post.category
+  }
+
+  isDisabled = () => {
+    const { title, body, author, category } = this.state
+
+    return '' === title.trim() || '' === body.trim() || '' === author.trim() || '' === category
+  }
+
+  handleChange = (e) => {
+    const target = e.target
+    const name = target.name
+    const value = target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    const { id, voteScore, deleted, commentCount } = this.props.post
+    const { title, body, author, category } = this.state
+
+    this.props.dispatch(editPost({
+      [id]: {
+        id,
+        timestamp: getTimestamp(),
+        title: title.trim(),
+        body: body.trim(),
+        author: author.trim(),
+        category,
+        voteScore,
+        deleted,
+        commentCount
+      }
+    }))
+  }
+
+  render() {
+    const { title, body, author, category } = this.state
+
+    return (
+      <PostForm
+        title={title}
+        body={body}
+        author={author}
+        category={category}
+        isDisabled={this.isDisabled}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    )
+  }
+}
+
+function mapStateToProps({ posts }, { id }) {
+  return {
+    post: posts[id]
+  }
+}
+
+export default connect(mapStateToProps)(EditPost)
