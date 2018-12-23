11725b20a10e16eb0988a26827f7a18ef3735286
mokajima <mokajima.85z@gmail.com>
2018-12-23 10:25:01 +0900

    feat: Add an EditComment component

---
+++ frontend/src/components/EditComment.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { editComment } from '../actions/comments'
+
+class EditComment extends Component {
+  state = {
+    author: this.props.comment.author,
+    body: this.props.comment.body
+  }
+
+  handleChange = (e) => {
+    const target = e.target
+    const name = target.name
+    const value = target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    const { id, parentId, voteScore, deleted, parentDeleted } = this.props.comment
+    const { author, body } = this.state
+
+    this.props.dispatch(editComment({
+      [id]: {
+        id,
+        parentId,
+        timestamp: new Date().getTime(),
+        body: body.trim(),
+        author: author.trim(),
+        voteScore,
+        deleted,
+        parentDeleted
+      }
+    }))
+  }
+
+  render() {
+    const { comment } = this.props
+    const { author, body } = this.state
+
+    return (
+      <div>
+        <input
+          type="text"
+          name="author"
+          value={author}
+          onChange={this.handleChange}
+        />
+        <textarea
+          name="body"
+          value={body}
+          onChange={this.handleChange}
+        />
+        <button
+          disabled={'' === author.trim() || '' === body.trim()}
+          onClick={this.handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ comments }, { id }) {
+  return {
+    comment: comments[id]
+  }
+}
+
+export default connect(mapStateToProps)(EditComment)
