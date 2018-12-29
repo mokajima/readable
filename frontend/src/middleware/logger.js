c53f2615ea030cbc7eddebfb39af58ef5f8e4b1d
mokajima <mokajima.85z@gmail.com>
2018-12-29 12:27:23 +0900

    style: Add comments for ESLint

--- frontend/src/middleware/logger.js
+++ frontend/src/middleware/logger.js
@@ -1,9 +1,13 @@
 const logger = (store) => (next) => (action) => {
+
+  /* eslint-disable indent, no-console */
   console.group(action.type)
     console.log('The action: ', action)
     const returnValue = next(action)
     console.log('The new state: ', store.getState())
   console.groupEnd()
+  /* eslint-enable indent, no-console */
+
   return returnValue
 }
 
