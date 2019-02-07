dc59e775cc5d65e8c9beb359001e71d6ab12b2a4
mokajima <mokajima.85z@gmail.com>
2019-02-08 07:21:35 +0900

    refactor: Use a function component instead of a class component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-class Header extends Component {
-  render() {
-    const { categories } = this.props
-
-    return (
-      <header className="header">
-        <div className="header__inner">
-          <Link className="logo" to="/">Readable</Link>
-          <ul className="categories-list">
-            {categories.map((category) => (
-              <li className="categories-list__item" key={category.name}>
-                <NavLink to={`/${category.path}`} activeStyle={{color: 'b40b43'}}>
-                  {category.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </header>
-    )
-  }
+function Header(props) {
+  return (
+    <header className="header">
+      <div className="header__inner">
+        <Link className="logo" to="/">Readable</Link>
+        <ul className="categories-list">
+          {props.categories.map((category) => (
+            <li className="categories-list__item" key={category.name}>
+              <NavLink to={`/${category.path}`} activeStyle={{color: 'b40b43'}}>
+                {category.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  )
 }
 
 Header.propTypes = {
