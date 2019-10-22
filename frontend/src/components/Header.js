931aac84f853430b94e19814244e658185d84c18
mokajima <mokajima85z@gmail.com>
2019-10-22 19:58:27 +0900

    refactor: Refactor the Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,25 +2,22 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-function Header(props) {
-  console.log(props)
-  return (
-    <header className="header">
-      <div className="header__inner">
-        <Link className="logo" to="/">Readable</Link>
-        <ul className="categories-list">
-          {props.categories.map((category) => (
-            <li className="categories-list__item" key={category.name}>
-              <NavLink to={`/${category.path}`} activeStyle={{color: 'b40b43'}}>
-                {category.name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </header>
-  )
-}
+const Header = ({ categories }) => (
+  <header className="header">
+    <div className="header__inner">
+      <Link className="logo" to="/">Readable</Link>
+      <ul className="categories-list">
+        {categories.map(category => (
+          <li className="categories-list__item" key={category.name}>
+            <NavLink to={`/${category.path}`} activeStyle={{color: 'b40b43'}}>
+              {category.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </header>
+)
 
 Header.propTypes = {
   categories: PropTypes.array.isRequired
