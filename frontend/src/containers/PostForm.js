ccd67a9b6702f12a0a67c09d099265f003e3855c
mokajima <mokajima85z@gmail.com>
2019-10-24 23:35:15 +0900

    refactor: Remove the PostForm container

--- frontend/src/containers/PostForm.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import PostForm from '../components/PostForm'
-
-const mapStateToProps = ({ categories }) => ({
-  categories
-})
-
-export default connect(mapStateToProps)(PostForm)
