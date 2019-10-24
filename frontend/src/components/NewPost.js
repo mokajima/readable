de4d5dd365ba90e14434042bfda30e8bba6b360e
mokajima <mokajima85z@gmail.com>
2019-10-24 23:34:24 +0900

    refactor: Refactor the NewPost page

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -1,87 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
-import { getId, getTimestamp } from '../utils/helpers'
-import PostForm from '../containers/PostForm'
+import PostForm from '../components/PostForm'
 
-class NewPost extends Component {
-  state = {
-    title: '',
-    body: '',
-    author: '',
-    category: 'react'
-  }
-
-  /**
-   * @description Whether or not all form fields are filled
-   * @returns {bool}
-   */
-  isDisabled = () => {
-    const { title, body, author, category } = this.state
-
-    return '' === title.trim() || '' === body.trim() || '' === author.trim() || '' === category
-  }
-
-  /**
-   * @description Update the state
-   * @params {object} - The event object
-   */
-  handleChange = (e) => {
-    const target = e.target
-    const name = target.name
-    const value = target.value
-
-    this.setState({
-      [name]: value
-    })
-  }
-
-  /**
-   * @description Add a new post
-   */
-  handleSubmit = () => {
-    const { title, body, author, category } = this.state
-    const id = getId()
-
-    const post = {
-      id,
-      timestamp: getTimestamp(),
-      title: title.trim(),
-      body: body.trim(),
-      author: author.trim(),
-      category,
-      voteScore: 1,
-      deleted: false,
-      commentCount: 0
-    }
-
-    this.props.addPost(post)
-  }
-
-  render() {
-    const { title, body, author, category } = this.state
-
-    return (
-      <>
-        <Helmet>
-          <title>Add New Post | Readable</title>
-        </Helmet>
-        <PostForm
-          title={title}
-          body={body}
-          author={author}
-          category={category}
-          isDisabled={this.isDisabled}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-      </>
-    )
-  }
-}
+const NewPost = ({
+  categories,
+  values,
+  handleChange,
+  handleSubmit
+}) => (
+  <>
+    <Helmet>
+      <title>Add New Post | Readable</title>
+    </Helmet>
+    <PostForm
+      categories={categories}
+      values={values}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  </>
+)
 
 NewPost.propTypes = {
-  addPost: PropTypes.func.isRequired
+  categories: PropTypes.array.isRequired,
+  values: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 }
 
 export default NewPost
--- frontend/src/containers/NewPost.js
+++ frontend/src/containers/NewPost.js
@@ -1,13 +1,47 @@
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { handleAddPost } from '../actions/posts'
+import { getId, getTimestamp } from '../utils/helpers'
 import NewPost from '../components/NewPost'
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  addPost: post => {
-    dispatch(handleAddPost(post))
-    ownProps.history.push(`/${post.category}/${post.id}`)
-  }
-})
+const NewPostContainer = () => {
+  const [values, setValues] = useState({
+    title: '',
+    body: '',
+    author: '',
+    category: 'react'
+  })
+  const categories = useSelector(state => state.categories)
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-export default withRouter(connect(null, mapDispatchToProps)(NewPost))
+  const handleChange = (targetName, newValue) => {
+    setValues(v => ({ ...v, [targetName]: newValue }))
+  }
+
+  const handleSubmit = () => {
+    const post = {
+      ...values,
+      id: getId(),
+      timestamp: getTimestamp(),
+      voteScore: 1,
+      deleted: false,
+      commentCount: 0
+    }
+
+    dispatch(handleAddPost(post))
+    history.push(`/${post.category}/${post.id}`)
+  }
+
+  return (
+    <NewPost
+      categories={categories}
+      values={values}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
+
+export default NewPostContainer
