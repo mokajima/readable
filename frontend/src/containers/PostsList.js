a3cbb24519977176a340968e8c55cfa7c5a201a7
mokajima <mokajima85z@gmail.com>
2019-10-26 22:59:09 +0900

    refactor: Refactor the PostsList container

--- frontend/src/containers/PostsList.js
+++ frontend/src/containers/PostsList.js
@@ -1,18 +1,66 @@
-import { connect } from 'react-redux'
+import React, { useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import * as sharedActions from '../actions/shared'
+import * as postsActions from '../actions/posts'
 import PostsList from '../components/PostsList'
 
-const mapStateToProps = ({ posts }, ownProps) => {
-  const { category } = ownProps.match.params
+const PostsListContainer = () => {
+  const [sort, setSort] = useState('newest')
+  const dispatch = useDispatch()
+  const posts = useSelector(state => state.posts)
+  const { category } = useParams()
 
-  const ids = category
-    ? Object.keys(posts).filter((id) => posts[id].category === category)
-    : Object.keys(posts)
+  const filteredPostIds = useMemo(() => {
+    const postIds = Object.keys(posts)
 
-  return {
-    posts,
-    ids,
-    category
+    if (category) {
+      return postIds.filter(id => posts[id].category === category)
+    }
+
+    return postIds
+  }, [category, posts])
+
+  const sortedPostIds = useMemo(() => {
+    switch (sort) {
+      case 'newest':
+        return filteredPostIds.sort((a, b) => posts[b].timestamp - posts[a].timestamp)
+      case 'oldest' :
+        return filteredPostIds.sort((a, b) => posts[a].timestamp - posts[b].timestamp)
+      case 'votes' :
+        return filteredPostIds.sort((a, b) => posts[b].voteScore - posts[a].voteScore)
+      default :
+        return filteredPostIds
+    }
+  }, [filteredPostIds, posts, sort])
+
+  const handleClick = sort => {
+    setSort(sort)
   }
+
+  const handleDeletePost = id => {
+    dispatch(sharedActions.handleDeletePost(id))
+  }
+
+  const handleUpPostVote = id => {
+    dispatch(postsActions.handleUpPostVote(id))
+  }
+
+  const handleDownPostVote = id => {
+    dispatch(postsActions.handleDownPostVote(id))
+  }
+
+  return (
+    <PostsList
+      category={category}
+      ids={sortedPostIds}
+      posts={posts}
+      handleClick={handleClick}
+      handleDeletePost={handleDeletePost}
+      handleUpPostVote={handleUpPostVote}
+      handleDownPostVote={handleDownPostVote}
+    />
+  )
 }
 
-export default connect(mapStateToProps)(PostsList)
+export default PostsListContainer
