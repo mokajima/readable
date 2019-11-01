c2bf161bd178571e27327a239c61bd361fbff236
mokajima <mokajima85z@gmail.com>
2019-11-02 06:47:33 +0800

    refactor: Refactor the NewComment component

--- frontend/src/components/NewComment.js
+++ frontend/src/components/NewComment.js
@@ -1,102 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { getId, getTimestamp } from '../utils/helpers'
 
-class NewComment extends Component {
-  state = {
-    author: '',
-    body: ''
-  }
-
-  /**
-   * @description Update the state
-   * @params {object} - The event object
-   */
-  handleChange = (e) => {
-    const target = e.target
-    const name = target.name
-    const value = target.value
-
-    this.setState({
-      [name]: value
-    })
-  }
-
-  /**
-   * @description Add a comment to a post
-   */
-  handleSubmit = () => {
-    const { parentId } = this.props
-    const { author, body } = this.state
-    const id = getId()
-
-    const comment = {
-      id,
-      parentId,
-      timestamp: getTimestamp(),
-      body: body.trim(),
-      author: author.trim(),
-      voteScore: 1,
-      deleted: false,
-      parentDeleted: false
-    }
-
-    this.props.addComment(comment)
-
-    this.setState({
-      author: '',
-      body: ''
-    })
-  }
-
-  render() {
-    const { author, body } = this.state
-
-    return (
-      <div>
-        <table className="table">
-          <caption className="table__caption">Create a comment</caption>
-          <tbody>
-            <tr>
-              <th>Author</th>
-              <td>
-                <input
-                  className="input"
-                  type="text"
-                  name="author"
-                  value={author}
-                  onChange={this.handleChange}
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>Body</th>
-              <td>
-                <textarea
-                  className="input"
-                  name="body"
-                  value={body}
-                  onChange={this.handleChange}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button
-          className="button"
-          disabled={'' === author.trim() || '' === body.trim()}
-          onClick={this.handleSubmit}
-        >
-          Submit
-        </button>
-      </div>
-    )
-  }
-}
+const NewComment = ({ disabled, values, handleChange, handleSubmit }) => (
+  <div>
+    <table className="table">
+      <caption className="table__caption">Create a comment</caption>
+      <tbody>
+        <tr>
+          <th>Author</th>
+          <td>
+            <input
+              className="input"
+              type="text"
+              value={values.author}
+              onChange={e => handleChange('author', e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Body</th>
+          <td>
+            <textarea
+              className="input"
+              value={values.body}
+              onChange={e => handleChange('body', e.target.value)}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button
+      className="button"
+      disabled={disabled}
+      onClick={handleSubmit}
+    >
+      Submit
+    </button>
+  </div>
+)
 
 NewComment.propTypes = {
-  parentId: PropTypes.string.isRequired,
-  addComment: PropTypes.func.isRequired
+  disabled: PropTypes.bool.isRequired,
+  values: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 }
 
 export default NewComment
