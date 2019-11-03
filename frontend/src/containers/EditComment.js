cd95da929c461a97e9b303d0fca5ca113ef1e3d0
mokajima <mokajima85z@gmail.com>
2019-11-03 20:19:58 +0800

    refactor: Refactor the EditComment container

--- frontend/src/containers/EditComment.js
+++ frontend/src/containers/EditComment.js
@@ -1,19 +1,53 @@
-import { connect } from 'react-redux'
+import React, { useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import { handleEditComment } from '../actions/comments'
 import EditComment from '../components/EditComment'
+import { getTimestamp } from '../utils/helpers'
 
-const mapStateToProps = ({ comments }, ownProps) => {
-  const { id } = ownProps.match.params
+const EditCommentContainer = () => {
+  const dispatch = useDispatch()
+  const comments = useSelector(state => state.comments)
+  const { id } = useParams()
 
-  return {
-    comment: comments[id]
+  const comment = comments[id]
+
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [values, setValues] = useState({
+    author: comment.author,
+    body: comment.body
+  })
+
+  const disabled = useMemo(() => {
+    const { author, body } = values
+    return author.trim() === '' || body.trim() === ''
+  }, [values])
+
+  const handleChange = (targetName, newValue) => {
+    setValues(v => ({ ...v, [targetName]: newValue }))
   }
+
+  const handleSubmit = () => {
+    setIsSubmitted(true)
+
+    const newComment = {
+      ...comment,
+      ...values,
+      timestamp: getTimestamp()
+    }
+
+    dispatch(handleEditComment(newComment))
+  }
+
+  return (
+    <EditComment
+      disabled={disabled}
+      isSubmitted={isSubmitted}
+      values={values}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  )
 }
 
-const mapDipatchToProps = dispatch => ({
-  editComment: comment => {
-    dispatch(handleEditComment(comment))
-  }
-})
-
-export default connect(mapStateToProps, mapDipatchToProps)(EditComment)
+export default EditCommentContainer
