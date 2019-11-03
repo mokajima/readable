9655d957cf62dba306ae4543abed41b84af61ebe
mokajima <mokajima85z@gmail.com>
2019-11-03 20:19:33 +0800

    refactor: Refactor the EditComment component

--- frontend/src/components/EditComment.js
+++ frontend/src/components/EditComment.js
@@ -1,118 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
-import { getTimestamp } from '../utils/helpers'
 
-class EditComment extends Component {
-  state = {
-    author: this.props.comment.author,
-    body: this.props.comment.body,
-    isSubmitted: false
-  }
-
-  /**
-   * @description Whether or not all form fields are filled
-   * @returns {bool}
-   */
-  isDisabled = () => {
-    const { author, body } = this.state
-
-    return '' === author.trim() || '' === body.trim()
-  }
-
-  /**
-   * @description Update the state
-   * @param {object} e - The event object
-   */
-  handleChange = (e) => {
-    const target = e.target
-    const name = target.name
-    const value = target.value
-
-    this.setState({
-      [name]: value,
-      isSubmitted: false
-    })
-  }
-
-  /**
-   * @description Save an edited comment
-   */
-  handleSubmit = () => {
-    const { id, parentId, voteScore, deleted, parentDeleted } = this.props.comment
-    const { author, body } = this.state
-
-    const comment = {
-      id,
-      parentId,
-      timestamp: getTimestamp(),
-      body: body.trim(),
-      author: author.trim(),
-      voteScore,
-      deleted,
-      parentDeleted
-    }
-
-    this.props.editComment(comment)
-
-    this.setState({
-      isSubmitted: true
-    })
-  }
-
-  render() {
-    const { author, body, isSubmitted } = this.state
-
-    return (
-      <>
-        <Helmet>
-          <title>Edit Comment | Readable</title>
-        </Helmet>
-        {isSubmitted && (
-          <p>Comment updated!</p>
-        )}
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Author</th>
-              <td>
-                <input
-                  className="input"
-                  type="text"
-                  name="author"
-                  value={author}
-                  onChange={this.handleChange}
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>Body</th>
-              <td>
-                <textarea
-                  className="input"
-                  name="body"
-                  value={body}
-                  onChange={this.handleChange}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button
-          className="button"
-          disabled={this.isDisabled()}
-          onClick={this.handleSubmit}
-        >
-          Submit
-        </button>
-      </>
-    )
-  }
-}
+const EditComment = ({
+  disabled,
+  isSubmitted,
+  values,
+  handleChange,
+  handleSubmit
+}) => (
+  <>
+    <Helmet>
+      <title>Edit Comment | Readable</title>
+    </Helmet>
+    {isSubmitted && (
+      <p>Comment updated!</p>
+    )}
+    <table className="table">
+      <tbody>
+        <tr>
+          <th>Author</th>
+          <td>
+            <input
+              className="input"
+              type="text"
+              value={values.author}
+              onChange={e => handleChange('author', e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Body</th>
+          <td>
+            <textarea
+              className="input"
+              value={values.body}
+              onChange={e => handleChange('body', e.target.value)}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button
+      className="button"
+      disabled={disabled}
+      onClick={handleSubmit}
+    >
+      Submit
+    </button>
+  </>
+)
 
 EditComment.propTypes = {
-  comment: PropTypes.object.isRequired,
-  editComment: PropTypes.func.isRequired
+  disabled: PropTypes.bool.isRequired,
+  values: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 }
 
 export default EditComment
